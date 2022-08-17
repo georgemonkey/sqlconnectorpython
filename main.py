c343a98dcd09024f710d230567f1405ed0de4bca
interestingstuf <111271071+interestingstuf@users.noreply.github.com>
2022-08-17 11:25:03 +0530

    file
    i added how to with comments

---
+++ main.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+import mysql.connector
+#^^^^Imports
+root=tk.Tk()
+root.geometry("300x200")
+#^^^Window Creation
+l1=tk.Label(root,text="Name:")
+l1.grid(row=0, column=0)
+t1=tk.Entry(root)
+t1.grid(row=0,column=1)
+
+l2=tk.Label(root,text="ID:")
+l2.grid(row=1, column=0)
+t2=tk.Entry(root,show="*")
+t2.grid(row=1,column=1)
+#^^^^^Creation Of Form
+def f1():
+    mydb=mysql.connector.connect(
+    host="localhost",
+    database="connection",
+    user="root",
+    password="tyu@3434",
+    
+
+    )
+    #connection of database
+    cursor=mydb.cursor()
+    print("Connection Successful")
+    name=t1.get()
+    id=t2.get()
+    sql=("insert into a (NAME,ID) values (%s, %s) ")
+    val=(name,id)
+    cursor.execute(sql,val)
+    mydb.commit()
+    myresult=cursor.fetchall()
+    for x in myresult:
+        print(x)
+    mydb.close()
+    #Inserts data into databbase
+b1=tk.Button(root,text="Enter Values",command=f1)
+b1.grid(row=3,column=0)
+#creates button
+
+root.mainloop()
